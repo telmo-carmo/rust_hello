f29d0e27e865a1c52adbfcb70badc65c7cb55ab4
Telmo Carmo <telmo.carmo@gmail.com>
2025-02-01 18:51:11 +0000

    usinh hex and rand crates

--- src/encChaCha20.rs
+++ src/encChaCha20.rs
@@ -1,16 +1,17 @@
 use chacha20::cipher::{KeyIvInit, StreamCipher};
 use chacha20::XChaCha20;
+use rand::RngCore;
 
-fn hex_to_bytes(str_hex: &str) -> Vec<u8> {
-    let mut bytes = Vec::new();
-    for i in (0..str_hex.len()).step_by(2) {
-        let byte = u8::from_str_radix(&str_hex[i..i + 2], 16).expect("Invalid hex string");
-        bytes.push(byte);
-    }
-    bytes
-}
+// fn hex_to_bytes(str_hex: &str) -> Vec<u8> {
+//     let mut bytes = Vec::new();
+//     for i in (0..str_hex.len()).step_by(2) {
+//         let byte = u8::from_str_radix(&str_hex[i..i + 2], 16).expect("Invalid hex string");
+//         bytes.push(byte);
+//     }
+//     bytes
+// }
 
-fn encrypt_string(plaintext: &str, key: &[u8; 32], iv: &[u8; 24]) -> Vec<u8> {
+fn encrypt_string(plaintext: &str, key: &[u8; 32], iv: &[u8; 24]) -> String {
     // Create cipher instance
     let mut cipher = XChaCha20::new(key.into(), iv.into());
 
@@ -20,15 +21,13 @@ fn encrypt_string(plaintext: &str, key: &[u8; 32], iv: &[u8; 24]) -> Vec<u8> {
     // Encrypt the data in-place
     cipher.apply_keystream(&mut ciphertext);
 
-    ciphertext
+    base64::encode(&ciphertext)
 }
 
-fn decrypt_string(ciphertext: &[u8], key: &[u8; 32], iv: &[u8; 24]) -> String {
+fn decrypt_string(ciphertext_b64: &str, key: &[u8; 32], iv: &[u8; 24]) -> String {
+    let mut plaintext = base64::decode(ciphertext_b64).expect("bad base64 string");
     // Create cipher instance (same key and IV as encryption)
     let mut cipher = XChaCha20::new(key.into(), iv.into());
-
-    // Create a copy of the ciphertext to decrypt (important: don't modify in-place if you need the original)
-    let mut plaintext = ciphertext.to_vec();
 
     // Decrypt the data in-place
     cipher.apply_keystream(&mut plaintext);
@@ -44,20 +43,23 @@ fn main() {
     } else {
         "This is a Very secret message!"
     };
-    // Generate a random 256-bit key and 192-bit IV (Initialization Vector).
+    // Generate a random192-bit IV (Initialization Vector).
 
-    let key_vec = hex_to_bytes("3af5f3d48ca94da0c57dd5062b86a0cd19f83cf48b566cee276f29a82c7f1537");
-    let iv_vec = hex_to_bytes("329ea73fae3f3ee91ded6ace33d33657e82a24eff56b725a");
-
-    let mut key = [0u8; 32];
+    let mut rng = rand::rng();
     let mut iv = [0u8; 24];
+    rng.fill_bytes(&mut iv);
+    println!("IV: {}",&base64::encode(iv));
 
+    // crate hex :  hex::decode("aabb")  or hex_to_bytes()
+    //  32by , 256-bit key :
+    let key_vec = hex::decode("3AF5f3d48ca94da0c57dd5062b86a0cd19f83cf48b566cee276f29a82c7f1537").expect("bad hex str");
+   
+    let mut key = [0u8; 32];
     key.copy_from_slice(&key_vec);
-    iv.copy_from_slice(&iv_vec);
+  
 
     let ciphertext = encrypt_string(plaintext, &key, &iv);
-
-    println!("Ciphertext: {:?}", ciphertext); // Print the ciphertext (bytes)
+    println!("Ciphertext: {}", &ciphertext); // Print the ciphertext (bytes) in b64
 
     let decrypted_text = decrypt_string(&ciphertext, &key, &iv);
 
