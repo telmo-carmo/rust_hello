2e529dafbbe5025ccad60749db2e248c0b68e2d9
Telmo Carmo <telmo.carmo@novobanco.pt>
2025-02-21 09:37:41 +0000

    now uses pico-args arg parser

--- src/encChaCha20.rs
+++ src/encChaCha20.rs
@@ -1,7 +1,4 @@
-/*
-using a stream cypher for strings
-
-*/
+use base64::{engine::general_purpose::STANDARD, Engine as _};
 use chacha20::cipher::{KeyIvInit, StreamCipher};
 use chacha20::XChaCha20;
 use rand::RngCore;
@@ -15,24 +12,30 @@ use rand::RngCore;
 //     bytes
 // }
 
-fn encrypt_string(plaintext: &str, key: &[u8; 32], iv: &[u8; 24]) -> String {
-    // Create cipher instance
-    let mut cipher = XChaCha20::new(key.into(), iv.into());
+fn encrypt_string(plaintext: &str, key: &[u8; 32]) -> String {
+    let mut rng = rand::rng();
+    let mut iv = [0u8; 24];
+    rng.fill_bytes(&mut iv);
+    // Create cipher instance Key 256 bits, Nonce 192 bits
+    let mut cipher = XChaCha20::new(key.into(), &iv.into());
 
     // Convert plaintext to bytes
     let mut ciphertext = plaintext.as_bytes().to_vec();
 
     // Encrypt the data in-place
     cipher.apply_keystream(&mut ciphertext);
-
-    base64::encode(&ciphertext)
+    let mut result = iv.to_vec();
+    result.extend(ciphertext);
+    STANDARD.encode(&result)
 }
 
-fn decrypt_string(ciphertext_b64: &str, key: &[u8; 32], iv: &[u8; 24]) -> String {
-    let mut plaintext = base64::decode(ciphertext_b64).expect("bad base64 string");
+fn decrypt_string(ciphertext_b64: &str, key: &[u8; 32]) -> String {
+    let vby = STANDARD.decode(ciphertext_b64).expect("bad base64 string");
+    let iv = &vby[..24];
+    let ctext = &vby[24..];
     // Create cipher instance (same key and IV as encryption)
     let mut cipher = XChaCha20::new(key.into(), iv.into());
-
+    let mut plaintext = ctext.to_vec();
     // Decrypt the data in-place
     cipher.apply_keystream(&mut plaintext);
 
@@ -47,51 +50,21 @@ fn main() {
     } else {
         "This is a Very secret message!"
     };
-    // Generate a random192-bit IV (Initialization Vector).
 
-    let mut rng = rand::rng();
-    let mut iv = [0u8; 24];
-    rng.fill_bytes(&mut iv);
-    let iv_b64 = &base64::encode(iv);
-    println!("Rnd IV: {}",&iv_b64);
+    const KEY_HEX: &str = "3AF5f3d48ca94da0c57dd5062b86a0cd19f83cf48b566cee276f29a82c7f1537";
 
     // crate hex :  hex::decode("aabb")  or hex_to_bytes()
     //  32by , 256-bit key :
-    let key_vec = hex::decode("3AF5f3d48ca94da0c57dd5062b86a0cd19f83cf48b566cee276f29a82c7f1537").expect("bad hex str");
-   
+    let key_vec = hex::decode(KEY_HEX).expect("bad hex str");
+
     let mut key = [0u8; 32];
     key.copy_from_slice(&key_vec);
-  
 
-    let ciphertext = encrypt_string(plaintext, &key, &iv);
+    let ciphertext = encrypt_string(plaintext, &key);
     println!("Ciphertext: {}", &ciphertext); // Print the ciphertext (bytes) in b64
-
-    let iv_vec = base64::decode(iv_b64).expect("b64 IV str");
-    let mut iv = [0u8; 24];
-    iv.copy_from_slice(&iv_vec);
-    let decrypted_text = decrypt_string(&ciphertext, &key, &iv);
+    let decrypted_text = decrypt_string(&ciphertext, &key);
 
     println!("Decrypted text: {}", decrypted_text); // Print the decrypted string
 
     assert_eq!(plaintext, decrypted_text); // Ensure that decryption worked correctly
-
-    let s1 = "2023.02.03";
-
-    // semantic Version packing:  if ver 1 > ver2 {}
-    let parts: Vec<&str> = s1.split('.').collect();
-    if parts.len() == 3 {
-        let year: u16 = parts[0].parse().expect("Invalid year");
-        let month: u8 = parts[1].parse().expect("Invalid month");
-        let day: u8 = parts[2].parse().expect("Invalid day");
-
-        let mut ver_bytes = [0u8; 4];
-        ver_bytes[0..2].copy_from_slice(&year.to_be_bytes());
-        ver_bytes[2] = month;
-        ver_bytes[3] = day;
-
-        let ver_packed = u32::from_be_bytes(ver_bytes);
-        println!("Packed version: {} - 0x{:X}", ver_packed, ver_packed);
-    } else {
-        println!("Invalid date format");
-    }
 }
---
+++ src/pargs.rs
@@ -0,0 +1,73 @@
+/*
+
+parsing command line args example app
+
+cargo add pico-args
+
+OR
+
+[dependencies]
+pico-args = { version = "0.5", features = ["eq-separator"] }   
+
+feature "combined-flags"  allows -abc instead of -a -b -c
+
+*/
+
+use pico_args::Arguments;
+
+const HELP_MSG: &str = "\
+App XX version 1.2
+
+USAGE:
+  app [OPTIONS] -t NUM  [INPUT]
+
+FLAGS:
+  -h, --help           Prints help information
+
+OPTIONS:
+  -t NUMBER            Sets a number
+  -n. --num NUMBER     Sets an optional number [default: 0]
+  -s, --str STRING     Sets an optional str
+  -v                   Sets verbose flag
+
+ARGS:
+  <INPUT>
+";
+
+fn main() {
+    let mut pargs = Arguments::from_env();
+
+    if pargs.contains(["-h", "--help"]) {
+        print!("{}", HELP_MSG);
+        std::process::exit(0);
+    }
+
+    match pargs.value_from_str::<&str, i32>("-t") {
+        Ok(t ) => println!("arg -t = {}", t),
+        Err(e) => {
+            eprintln!("{} - Missing -t arg", e);
+            std::process::exit(1);
+        }
+    }
+
+    let nv: i32 = pargs
+        .opt_value_from_str(["-n", "--num"])
+        .unwrap()
+        .unwrap_or(0);
+
+    let name: String = pargs
+        .opt_value_from_str(["-s", "--str"])
+        .unwrap()
+        .unwrap_or_else(|| "None".to_string());
+
+    let vf: bool = pargs.contains("-v");
+    let remaining_args = pargs.finish();
+
+    println!("Integer argument: {}", nv);
+    println!("String argument: {}", name);
+    println!("Bool argument: {}", vf);
+    if !remaining_args.is_empty() {
+        println!("Remaining arguments: {:?}", remaining_args);
+        println!("INPUT_1 = {}", remaining_args[0].to_str().unwrap());
+    }
+}
